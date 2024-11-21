196929c5c63274445ed8a07b224bf9473702734e
lihsousa <lidianosousa@gmail.com>
2024-11-20 21:50:59 -0300

    Aprendendo Listas

--- Aulas_Js/main.js
+++ Aulas_Js/main.js
@@ -2,9 +2,9 @@ function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
+listaDeTeclas[0].onclick = tocaSomPom;
 
 
 
