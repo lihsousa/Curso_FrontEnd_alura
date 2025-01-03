c48c907ac20c3e8e6da6316b40edc4c5bea32c57
lihsousa <lidianosousa@gmail.com>
2025-01-03 14:05:35 -0300

    Then e Json

--- Aulas_Js/projeto_alura_books/script.js
+++ Aulas_Js/projeto_alura_books/script.js
@@ -1,3 +1,5 @@
-consultaCEP = fetch('https://viacep.com.br/ws/01001000/json');
+consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
+    .then(resposta => resposta.json())
+    .then(r => console.log(r));
 
 console.log(consultaCEP);
